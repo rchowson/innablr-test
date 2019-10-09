af5156ceff3223b25f83d2b46acda3dd29bdaefc
Robert Howson <rchowson@yahoo.com>
2019-10-09 22:13:20 +1100

    added build.sh script to be run by travis

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TRAVIS_COMMIT=$1
+
+STATUS_FILE="/usr/src/app/status.json"
+update_status() {
+    echo "{
+  "myapplication": [
+    {
+      "version": "1.0",
+      "description": "pre-interview technical test",
+      "lastcommitsha": "${TRAVIS_COMMIT}"
+    }
+  ]
+} > ${STATUS_FILE}
+    "
+}
+
+update_status
